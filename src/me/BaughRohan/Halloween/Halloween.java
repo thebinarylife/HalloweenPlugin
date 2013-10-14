27c400abe788d81f965a96344b519cc916b4cfa3
baugh70 <zakkyzakkba@gmail.com>
2013-10-14 02:19:56 -0400

    Didn't add permission as an if but added to the permissions class

--- src/me/BaughRohan/Halloween/Halloween.java
+++ src/me/BaughRohan/Halloween/Halloween.java
@@ -15,25 +15,29 @@ import org.bukkit.permissions.*;
 
 public class Halloween extends JavaPlugin {
 	Logger logger = getLogger();
-	
-	public void onEnable(){
-		 logger.info("Enabling Halloween by Baugh70 and Rohan576!");
+	private Permissions perms = new Permissions();
+
+	public void onEnable() {
+		logger.info("Enabling Halloween by Baugh70 and Rohan576!");
 	}
+
 	public void onDisable() {
 		logger.info("Halloween is now disabled.");
 	}
-	
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+
+	public boolean onCommand(CommandSender sender, Command cmd,
+			String commandLabel, String[] args) {
 		Player player = (Player) sender;
-		
-		
-		if (commandLabel.equalsIgnoreCase("halloween") || commandLabel.equalsIgnoreCase("hw")) {
+
+		if (commandLabel.equalsIgnoreCase("halloween")
+				|| commandLabel.equalsIgnoreCase("hw")) {
 			String halCmd = args[0];
+			
 			if (halCmd.equalsIgnoreCase("challenges") || halCmd.equalsIgnoreCase("help") || args.length == 0) {
 				player.sendMessage(ChatColor.BLACK + "Halloween Challenges:");
 				player.sendMessage(ChatColor.GOLD + "Pumpkin Picker: Get 10 pumpkins");
 				player.sendMessage(ChatColor.GOLD + "Pumpkin Carver: Get 20  Jack-o-Lanterns");
-			} else if(args.length > 1){
+				} else if (args.length > 1) {
 				player.sendMessage(ChatColor.DARK_RED + "Do /halloween for help!");
 			}
 			if (halCmd.equalsIgnoreCase("pumpkinpicker")) {
@@ -41,30 +45,29 @@ public class Halloween extends JavaPlugin {
 					player.getInventory().addItem(new ItemStack(Material.CAKE_BLOCK, 15));
 					player.getInventory().remove(new ItemStack(Material.PUMPKIN, 10));
 					Bukkit.broadcastMessage(ChatColor.GRAY + "" + player + ChatColor.YELLOW + " completed the Pumpkin Picker challenge!");
-				}
-				else {
+				} else {
 					player.sendMessage(ChatColor.GOLD + "Pumpkin Picker: Gather 10 Pumpkins. Reward: 15 Cake");
-				}}else if(args.length == 0){
-					player.sendMessage(ChatColor.BLACK + "Halloween Challenges:");
-					player.sendMessage(ChatColor.GOLD + "Pumpkin Picker: Get 10 pumpkins");
-					player.sendMessage(ChatColor.GOLD + "Pumpkin Carver: Get 20  Jack-o-Lanterns");
-				}else{
 				}
-				
-			if(halCmd.equalsIgnoreCase("pumpkincarver")){
-				if(player.getInventory().containsAtLeast(new ItemStack(Material.JACK_O_LANTERN), 15)){
+			} else if (args.length == 0) {
+				player.sendMessage(ChatColor.GRAY + "Halloween Challenges:");
+				player.sendMessage(ChatColor.GOLD + "Pumpkin Picker: Get 10 pumpkins");
+				player.sendMessage(ChatColor.GOLD + "Pumpkin Carver: Get 20  Jack-o-Lanterns");
+			} else {
+			}
+
+			if (halCmd.equalsIgnoreCase("pumpkincarver")) {
+				if (player.getInventory().containsAtLeast(new ItemStack(Material.JACK_O_LANTERN), 15)) {
 					player.getInventory().addItem(new ItemStack(Material.DIAMOND, 1));
 					player.getInventory().remove(new ItemStack(Material.PUMPKIN, 10));
 					Bukkit.broadcastMessage(ChatColor.BLACK + "" + player + ChatColor.GOLD + " completed the Punpkin Picker challenge!");
-				}
-				else{
+				} else {
 					player.sendMessage(ChatColor.GOLD + "Pumpkin Carver: Craft 15 Jack-O-Lanterns. Reward: 1 Diamond.");
 				}
 			}
-		
 		}
-		return false;
 		
+		return false;
+
 	}
-	
+
 }
