e5a756a3fee8a5efcfba493f4cf1ef19c7675983
rohan576 <rohan_vn@yahoo.com>
2013-10-13 20:32:50 -0700

    Added Plugin Base.
    Added a Halloween command.

---
+++ src/me/BaughPkerb0yRohan/Halloween/Halloween.java
@@ -0,0 +1,53 @@
+package me.baugh70.pkerb0y.Halloween;
+
+import java.security.Permissions;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class Main extends JavaPlugin {
+	Logger logger;
+	private Permissions perms = new Permissions();
+	ItemStack cake = new ItemStack(Material.CAKE_BLOCK, 15);
+	
+	public void onEnable(){
+		 logger.log(Level.INFO, "Version " + getDescription().getVersion() + " enabled!");
+	}
+	public void onDisable() {
+		this.logger = this.getLogger();
+		this.logger.info("Halloween is now disabled.");
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		Player player = (Player) sender;
+		
+		
+		if (commandLabel.equalsIgnoreCase("halloween") || commandLabel.equalsIgnoreCase("h")) {
+			String halCmd = args[0];
+			if (halCmd.equalsIgnoreCase("challenges") || halCmd.equalsIgnoreCase("help") || args.length == 0) {
+				player.sendMessage(ChatColor.DARK_AQUA + "Halloween Challenges:");
+				player.sendMessage(ChatColor.DARK_GREEN + "Pumpkin Picker: Get 10 pumpkins");
+			}else if(args.length > 1){
+				player.sendMessage(ChatColor.DARK_RED + "Do /halloween for help!");
+			}
+			if (halCmd.equalsIgnoreCase("pumpkinpicker")) {
+					player.getInventory().contains(Material.PUMPKIN);
+					player.getInventory().addItem(cake);
+					
+			}
+		
+		}
+		return false;
+		
+	}
+	
+}
