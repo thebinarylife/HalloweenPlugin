9213b80a6a1cea618e73020d6ad2d15dc29f75fd
baugh70 <zakkyzakkba@gmail.com>
2013-10-25 10:53:33 -0400

    Created permission variable

--- src/me/BaughRohanPkerb0y/Halloween/Permissions.java
+++ src/me/BaughRohanPkerb0y/Halloween/Permissions.java
@@ -3,5 +3,5 @@ package me.BaughRohanPkerb0y.Halloween;
 import org.bukkit.permissions.Permission;
 
 public class Permissions {
-	public Permission challenges = new Permission("halloween.challenges");
+	public static Permission challenges = new Permission("halloween.challenges");
 }
