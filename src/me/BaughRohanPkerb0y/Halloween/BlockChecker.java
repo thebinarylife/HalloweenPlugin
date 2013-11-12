a113baaf901a5810db514ed5e118d2cfbd30223a
baugh70 <zakkyzakkba@gmail.com>
2013-11-12 13:20:37 -0800

    Update BlockChecker.java

--- src/me/BaughRohanPkerb0y/Halloween/BlockChecker.java
+++ src/me/BaughRohanPkerb0y/Halloween/BlockChecker.java
@@ -1,4 +1,13 @@
-public class BlockChecker extends JavaPlugin{
+import org.bukkit.plugin.java.JavaPlugin
 
+public class BlockChecker extends JavaPlugin{
+  private double playerCursor;
+  
+  public static getPlayerCursor(Player player){
+    Player player = (Player) player;
+    
+    
+    
+  }
 
 }
